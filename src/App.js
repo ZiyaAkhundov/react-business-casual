e849e4418fd87238de3b0fb40fbb28ff4b1ffab5
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-06-09 19:55:42 +0400

    remove Navigate from react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Nav from './components/nav';
 import Main from './pages/main';
