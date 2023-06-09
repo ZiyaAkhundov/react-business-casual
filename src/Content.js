5744bdd9762e9388b8c038a430d0144999b2e263
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-06-09 19:55:03 +0400

    remove Content.js

--- src/Content.js
+++
@@ -1,9 +0,0 @@
-// import { Route, Routes} from 'react-router-dom';
-// import Main from './pages/main'
-// function Content(){
-//     return(
-//         <Routes>
-//             <Route path="/Main" element={<Main/>}/>
-//         </Routes>
-//     )
-// }
